620646958c24eaba888dc3f095d34464ab4ebbcd
Inaruslynx <inaruslynx@gmail.com>
2021-01-14 21:07:33 -0600

    Made some changes, but not ready

--- SmtApi.py
+++ SmtApi.py
@@ -3,38 +3,47 @@ import requests
 import session
 import exceptions
 
+
 class SmtApi(object):
 
-    def __init__(self, user:str, pwd:str, cert:Tuple, esiid, host='https://services.smartmetertexas.net') -> None:
-        super().__init__()
+    def __init__(self, user: str, pwd: str, cert: Tuple, esiid: int, host='https://services.smartmetertexas.net') -> None:
+        """ 
+        Instantiate a new API client.
+        Args:
+            user (str): username for API log in
+            pwd (str): password for API log in
+            cert (Tuple): location to certification and key files
+            esiid (int): esiid associated with account
+            host (str): base of API to be joined to API functions
+         """
         ####################
         # may not need to retain
         ####################
         self.user = user
         self.pwd = pwd
         ####################
-        self.url=host
-        self.esiid=esiid
+        self.host = host
+        self.esiid = esiid
         # Initialize the session.
-        self.session=SmtApiSession()
+        self.session = SmtApiSession()
         self.session.init_basic_auth(user, pwd, cert)
 
     # Perform an API request.
-    def _request(self, method, params=None, id=0):
+    def _request(self, params=None, id=0):
         data = {
             'trans_id': id,
             'requestorID': self.user,
             'requesterType': 'RES',
-            'version': 'L'
+            'version': 'L',
             'esiid': self.esiid,
             'SMTTermsandConditions': 'Y'
         }
         if params:
             data['params'] = params
 
-        # Ask the session to perform a JSON-RPC request 
+        # Ask the session to perform a JSON-RPC request
         # with the parameters provided.
-        resp = self.session.request('POST', self.url, json=data)
+        resp = self.session.request('POST', self.host, json=data)
 
         # If something goes wrong, we'll pass the response
         # off to the error-handling code
@@ -43,6 +52,10 @@ class SmtApi(object):
 
         # Otherwise return the result dictionary.
         return resp.json()['result']
+
+ # url helper
+    def _url(self, path):
+        return self.host + '/{:d}/'.format(path)
 
     # API methods
     # all methods require 'startDate' and 'endDate'
@@ -61,4 +74,3 @@ class SmtApi(object):
         return self._request('commit-entry', {
             'message': message
         })
-    
